ea4921de891579fad92c25df707ca4bc4cf81535
rkalavak <kalavakurirenu@gmail.com>
2019-05-13 06:47:47 +0530

    Initial commit

---
+++ src/main/java/com/kalavakuri/webapp/TestServlet.java
@@ -0,0 +1,35 @@
+package com.kalavakuri.webapp;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.kalavakuri.service.TestServletService;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+@WebServlet("/testservlet")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		TestServletService service = new TestServletService();
+		service.print();
+	}
+
+}

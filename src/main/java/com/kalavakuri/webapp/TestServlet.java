dc51b6c7de6c946d1e31cb1dc9cc4dec152a78fc
rkalavak <kalavakurirenu@gmail.com>
2019-05-15 19:16:48 +0530

    Changing from print1 to print2.

--- src/main/java/com/kalavakuri/webapp/TestServlet.java
+++ src/main/java/com/kalavakuri/webapp/TestServlet.java
@@ -29,7 +29,7 @@ public class TestServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		TestServletService service = new TestServletService();
-		service.print2();
+		service.print1();
 	}
 
 }
